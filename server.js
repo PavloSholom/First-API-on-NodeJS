431b1e0735963af7a11d7b474cfc230fdb943921
Pavlo Sholom <pavlo.sholom@gmail.com>
2018-06-27 13:00:49 +0300

    3. DB conection to Express is realized

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-var express = require('express'),
-    app = express();
-
+var express = require('express');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+
+var app = express();
+var db;
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
@@ -25,25 +29,37 @@ app.get('/',function(req, res) {
 });
 
 app.get('/artists',function(req, res) {
-  res.send(artists);
+  db.collection('artists').find().toArray(function(err, docs) {
+    if(err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(docs);
+  });
 });
 
 app.get('/artists/:id',function(req, res) {
-  var artistId = req.params.id;
-  var artist = artists.find(function(artist) {
-    return artist.id === +artistId
+  db.collection('artists').findOne({_id: ObjectID(req.params.id)}, function(err, doc) {
+    if(err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(doc);
   });
-  res.send(artist);
 });
 
 app.post('/artists', function(req, res) {
   console.log(req.body);
   var artist = {
-    id: Date.now(),
     name: req.body.name
   };
-  artists.push(artist);
-  res.send(artist);
+  db.collection('artists').insert(artist, function(err, result) {
+    if(err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(artist);
+  });
 });
 
 app.put('/artists/:id', function(req, res) {
@@ -63,7 +79,12 @@ app.delete('/artists/:id', function(req, res) {
   res.sendStatus(200);
 });
 
-
-app.listen(3333, function() {
-  console.log('Server started!');
+MongoClient.connect('mongodb://localhost:27017/', function(err, database) {
+  if(err) {
+    return console.log(err);
+  }
+  db = database.db("myapi");
+  app.listen(3333, function() {
+    console.log('Server started!');
+  })
 })
