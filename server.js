1b3955681bad40b6175749ab3e5280e6aa083509
Pavlo Sholom <pavlo.sholom@gmail.com>
2018-06-27 11:51:09 +0300

    2. Data adding and data refresh are realized

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express'),
     app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 var artists = [
   {
     id: 1,
@@ -31,6 +35,34 @@ app.get('/artists/:id',function(req, res) {
   });
   res.send(artist);
 });
+
+app.post('/artists', function(req, res) {
+  console.log(req.body);
+  var artist = {
+    id: Date.now(),
+    name: req.body.name
+  };
+  artists.push(artist);
+  res.send(artist);
+});
+
+app.put('/artists/:id', function(req, res) {
+  var artistId = req.params.id;
+  var artist = artists.find(function(artist) {
+    return artist.id === +artistId
+  });
+  artist.name = req.body.name;
+  res.sendStatus(200);
+});
+
+app.delete('/artists/:id', function(req, res) {
+  var artistId = req.params.id;
+  artists = artists.filter(function(artist) {
+    return artist.id !== +artistId
+  });
+  res.sendStatus(200);
+});
+
 
 app.listen(3333, function() {
   console.log('Server started!');
